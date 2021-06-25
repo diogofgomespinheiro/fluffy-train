14668834ab389c35ae9802a01e3e907dc4e171bb
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-06-25 20:41:37 +0100

    feat: create dato-cms queries

---
+++ src/lib/dato-cms/queries.ts
@@ -0,0 +1,99 @@
+export const landingPageQuery = `
+  landingPage {
+    seo: _seoMetaTags {
+      ...metaTagsFragment
+    }
+    navbarSelectedItemIndex
+    navbarItems {
+      pageId
+      label
+      route
+      spy
+      actionType
+      offset
+    }
+  }
+  site: _site {
+    favicon: faviconMetaTags {
+      ...metaTagsFragment
+    }
+  }
+`;
+
+export const homeSectionQuery = `
+  homeSection {
+    title
+    subTitle
+    socialLinks {
+      platform
+      icon {
+        url
+        height
+        width
+        alt
+      }
+    }
+    image {
+      url
+      alt
+      responsiveImage(imgixParams: {fm: png, fit: crop, w: 400, h: 400 }) {
+        ...responsiveImageFragment
+      }
+    }
+  }
+`;
+
+export const aboutSectionQuery = `
+  aboutSection {
+    title
+    description
+    qualifications {
+      title
+      items {
+        title
+        description
+      }
+    }
+  }
+`;
+
+export const projectsSectionQuery = `
+  projectsSection {
+    title
+    projects {
+      title
+      description
+      coverImage {
+        url
+        alt
+        responsiveImage(imgixParams: {fm: jpg, fit: crop, w: 500, h: 350 }) {
+          ...responsiveImageFragment
+        }
+      }
+      buttons {
+        text
+        url
+      }
+      techs {
+        name
+        image {
+          alt
+          url
+          width
+          height
+        }
+      }
+    }
+  }
+`;
+
+export const contactSectionQuery = `
+  contactSection {
+    title
+    description
+    buttons {
+      text
+      url
+    }
+  }
+`;
